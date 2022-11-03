283ef4f0fdb23ca29185515a90a45698f3ca55d5
nhmgtvt <nhmgtvt@gmail.com>
2022-11-03 18:56:53 +1000

    Cypress test

--- client/e2e/cypress/integration/cart-actions.spec.js
+++ client/e2e/cypress/integration/cart-actions.spec.js
@@ -11,7 +11,18 @@ context('Cart Actions', () => {
 
     cy.get('[data-cy=view-cart-btn]').click();
     cy.contains('2 Selected item')
-
+    
   })
 
+  it.only('Purchase items in cart', () => {
+    cy.get('[data-cy=add-to-cart-2]').click();
+    cy.get('[data-cy=add-to-cart-3]').click();
+
+    cy.get('[data-cy=view-cart-btn]').click();
+    
+    cy.get('[data-cy=purchase-cart]').click();
+    cy.get('[data-cy=show-recent-purchase]').click();
+    cy.get('[data-cy=recent-purchase-2]').should('be.visible');
+    cy.get('[data-cy=recent-purchase-3]').should('be.visible');
+  })
 })
